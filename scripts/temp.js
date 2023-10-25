02284c0adc7f322f74ad26719ea72718e584c7d5
LeilaC1 <leilaclesca13@gmail.com>
2023-10-24 21:17:54 -0400

    added input type removed reset btn

--- scripts/temp.js
+++ scripts/temp.js
@@ -2,31 +2,33 @@
 window.onload = init;
 
 function init() {
-    const covertBtn = document.getElementById("convertBtn");
-    convertBtn.onclick = onConvertBtnClicked;
+    const covertFBtn = document.getElementById("convertFBtn");
+    convertFBtn.onclick = onConvertFBtnClicked;
+    const covertCBtn = document.getElementById("convertCBtn");
+    convertCBtn.onclick = onConvertCBtnClicked;
+
 }
 
-function onConvertBtnClicked() {
-    const inputCelsius = document.getElementById("inputCelsius");
+function onConvertFBtnClicked() {
     const inputFahrenheit = document.getElementById("inputFahrenheit");
     const answerField5 = document.getElementById("answerField5");
     
 
     let fahrenheit = Number(inputFahrenheit.value);
-    let celsius = Number(inputCelsius.value);
-
-
-
-   
 
     let outputAnswerField5 = (fahrenheit - 32) * 5 / 9;
-    let outputAnswerField6 =  (celsius * 9 / 5) + 32;
-    answerField5.innerHTML = outputAnswerField5;
-    answerField6.innerHTML = outputAnswerField6;
+    answerField5.value = outputAnswerField5.toFixed(2);
 
 }
+function onConvertCBtnClicked(){
+    const inputCelsius = document.getElementById("inputCelsius");
+    const answerField6 = document.getElementById("answerField6");
 
-
+let celsius = Number(inputCelsius.value);
+    let outputAnswerField6 =  (celsius * 9 / 5) + 32;
+    answerField6.value = outputAnswerField6.toFixed(2);
+    
+}
 
 
 
